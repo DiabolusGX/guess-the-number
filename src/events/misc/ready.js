9c6c322a9d27584c5e06055418f0eba02184bc41
Ankit Malik <ankit.malik1092@gmail.com>
2021-03-21 03:09:49 +0530

    remove wishes

--- src/events/misc/ready.js
+++ src/events/misc/ready.js
@@ -2,14 +2,6 @@ const database = require("../../database/database");
 const configDoc = require("../../utils/configDoc");
 const dataDoc = require("../../utils/dataDoc");
 
-const activities_list = [
-    "your guesses || gg help",
-    "birthday wishes for ${dev} || Join server to wish || gg invite for link",
-    "join DEV Studios & wish ${dev} happy birthday || gg invite for link",
-    "HAPPY BIRTHDAY ${dev}",
-    "wishes for ${dev} on his birthday || gg invite for server link"
-];
-
 module.exports = async (client) => {
     await database();
     client.guilds.cache.forEach(async guild => {
@@ -18,10 +10,6 @@ module.exports = async (client) => {
         if (data.runningGames) data.runningGames.forEach((value, key) => client.games.set(key, { answer: value.answer, guesses: 0 }));
     });
 
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-        client.user.setActivity(activities_list[index], { type: "LISTENING" });
-    }, 30000);
-    //client.user.setActivity("your guesses || gg help", { type: "LISTENING" });
+    client.user.setActivity("your guesses || gg help", { type: "LISTENING" });
     console.log(client.user.tag + " is up and running in " + client.guilds.cache.size + " servers!");
 }
